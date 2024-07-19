670c08cd0f3292825285b47fb309359d1a133e99
bvo4 <brandonvo1980@yahoo.com>
2024-07-19 18:53:17 -0500

    Managed to get Mudblazor working and upgraded to .Net 8

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using BlazorGHPages.Logic;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorGHPages;
-
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IDependencyLogic, BetterDependencyLogic>();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
