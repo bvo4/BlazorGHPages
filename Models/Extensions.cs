28df808581f2d2c354a21458ca303e105cc1d3a7
bvo4 <55567877+bvo4@users.noreply.github.com>
2024-07-24 16:48:44 -0500

    Update Extensions.cs

--- Models/Extensions.cs
+++ Models/Extensions.cs
@@ -14,5 +14,39 @@ namespace BlazorGHPages.Models
 			var list = new List<string>();
 			list = config.GetSection("Software").Get<List<string>>();
 		}
+        //Read Appsetting.json
+        //private static readonly IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        public static string GET_WHHIST_CONN()
+        {
+            var builder = new ConfigurationBuilder()
+             .AddJsonFile($"appsettings.json", true, true);
+
+            var config = builder.Build();
+
+            var String = config["WH_HIST"];
+            return String;
+        }
+
+        public static string GET_WHNEW_CONN()
+        {
+            var builder = new ConfigurationBuilder()
+             .AddJsonFile($"appsettings.json", true, true);
+
+            var config = builder.Build();
+
+            var String = config["ConnectionString"];
+            return String;
+        }
+
+        public static List<T> ConvertFromJson<T>(this string json)
+        {
+            List<T> Temp = new List<T>();
+            if (json.StartsWith("["))    //Check if the returned json string was an array
+                Temp.Add(json.Deserialize<T>());
+            else
+                Temp = json.Deserialize<List<T>>();  //Deserialize back into data
+
+            return Temp;
+        }
     }
 }
