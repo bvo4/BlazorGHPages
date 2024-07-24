80bf3e2d4514ca579f1c12e71cc79fc64050c407
bvo4 <55567877+bvo4@users.noreply.github.com>
2024-07-24 16:54:27 -0500

    Update Extensions.cs

--- Models/Extensions.cs
+++ Models/Extensions.cs
@@ -38,6 +38,25 @@ namespace BlazorGHPages.Models
             return String;
         }
 
+	public static string GetDescription(this Enum value)
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+            if (name != null)
+            {
+                FieldInfo field = type.GetField(name);
+                if (field != null)
+                {
+                    DescriptionAttribute attr =
+                           Attribute.GetCustomAttribute(field,
+                             typeof(DescriptionAttribute)) as DescriptionAttribute;
+                    if (attr != null)
+                        return attr.Description;
+                }
+            }
+            return string.Empty;
+        }
+	    
         public static List<T> ConvertFromJson<T>(this string json)
         {
             List<T> Temp = new List<T>();
